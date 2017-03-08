64ee9c1b831c8cfff94b83eb88f02e1a8a2a586f
guilleasz <guille_aszyn@hotmail.com>
2017-03-08 16:59:23 -0300

    accept lowdashes for bold and italic

--- decoders.js
+++ decoders.js
@@ -171,7 +171,7 @@ export function orderList(string) {
 
 
 export function blocks(string) {
-  const arr = string.split('\n');
+  const arr = string.replace(/__/g, '**').replace(/_/g, '*').split('\n');
   for (let i = 0; i < arr.length; i += 1) {
     if (arr[i][0] === '#') {
       arr[i] = `${header(arr[i])}\n`;
--- dist/decoders.js
+++ dist/decoders.js
@@ -175,7 +175,7 @@ function orderList(string) {
 }
 
 function blocks(string) {
-  var arr = string.split('\n');
+  var arr = string.replace(/__/g, '**').replace(/_/g, '*').split('\n');
   for (var i = 0; i < arr.length; i += 1) {
     if (arr[i][0] === '#') {
       arr[i] = header(arr[i]) + '\n';
